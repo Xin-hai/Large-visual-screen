7e154f341173074f7eaaf2c4eb4c0f1cc075a1ef
Xin-hai <aa1940358283@163.com>
2023-02-06 21:09:10 +0800

    上传模块4

---
+++ src/components/chart-4.tsx
@@ -0,0 +1,54 @@
+import React, {useEffect, useRef} from 'react';
+import * as echarts from "echarts";
+import {px} from "../shared/px";
+import {createEchartsOptions} from "../shared/create-echarts-options";
+
+export const Chart4 = () => {
+  const divRef = useRef(null);
+  useEffect(() => {
+    var myChart = echarts.init(divRef.current);
+    myChart.setOption(createEchartsOptions({
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
+        splitLine: {show: true, lineStyle: {color: '#073E78'}},
+        axisTick: {show: false},
+        axisLine: {show: false},
+      },
+      yAxis: {
+        type: 'value',
+        splitLine: {lineStyle: {color: '#073E78'}},
+        axisLabel: {
+          formatter(val) {
+            return val * 100 + '%';
+          }
+        }
+      },
+      series: [{
+        name: '盗窃',
+        type: 'line',
+        data: [0.08,0.09,0.11,0.06,0.09,0.10,0.06,0.07,0.04,0.09,0.08,0.07,0.06],
+        symbol: 'circle',
+        symbolSize: px(12),
+        lineStyle: {width: px(2)},
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+            offset: 0,
+            color: '#414a9d'
+          }, {
+            offset: 1,
+            color: '#1b1d52'
+          }]),
+        }
+      }]
+    }));
+  }, []);
+
+  return (
+    <div className="bordered 案发时段">
+      <h2>案发时段分析</h2>
+      <div ref={divRef} className="chart"/>
+    </div>
+  );
+};
